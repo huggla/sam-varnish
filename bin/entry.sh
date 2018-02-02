9e0ef83c2452eb272806685e11edc90e12c46399
huggla <henrik.uggla@kristianstad.se>
2018-02-02 13:04:34 +0100

    Update entry.sh

--- bin/entry.sh
+++ bin/entry.sh
@@ -1,24 +1,82 @@
 !/bin/sh
 set -e
-IFS=","
 
 if [ ! -e "$VCL_FILE" ]
 then
    mkdir -p "$(dirname "$VCL_FILE")"
    if [ -n "$BACKENDS" ]
    then
+      echo "vcl 4.0;" >> "$VCL_FILE"
+      echo >> "$VCL_FILE"
+      IFS=";"
+      for import in $IMPORTS
+      do
+         echo "import $import;" >> "$VCL_FILE"
+      done
+      echo >> "$VCL_FILE"
       for backend in $BACKENDS
       do
          echo "backend $backend {" >> "$VCL_FILE"
          eval "backend_config=\$$backend"
          for row in $backend_config
          do
-            echo "   $row" >> "$VCL_FILE"
+            echo "   $row;" >> "$VCL_FILE"
          done
          echo "}" >> "$VCL_FILE"
          echo >> "$VCL_FILE"
       done
+      echo >> "$VCL_FILE"
+      for acl in $ACLS
+      do
+         echo "acl $acl {" >> "$VCL_FILE"
+         eval "acl_config=\$$acl"
+         for row in $acl_config
+         do
+            echo "   $row;" >> "$VCL_FILE"
+         done
+         echo "}" >> "$VCL_FILE"
+         echo >> "$VCL_FILE"
+      done
+      subs="VCL_INIT; VCL_RECV; VCL_PIPE; VCL_PASS; VCL_HASH; VCL_HIT; VCL_MISS; VCL_BACKEND_RESPONSE; VCL_DELIVER; VCL_PURGE; VCL_SYNTH; VCL_FINI"
+      for sub in $subs
+      do
+         sub_lc=`tr "[:upper:]" "[:lower:]" $sub`
+         echo "sub $sub_lc {" >> "$VCL_FILE"
+         eval "sub_rows=\$$sub"
+         for row in $sub_rows
+         do
+            echo "   $row;" >> "$VCL_FILE"
+         done
+         echo "}" >> "$VCL_FILE"
+         echo >> "$VCL_FILE"
+      done
+   else
       
+fi
+         
+         
+
+
+
+
+
+      subs="
+      echo "sub vcl_recv {" >> "$VCL_FILE"
+      IFS=";"
+      for row in $VCL_RECV
+      do
+         echo "   $row;" >> "$VCL_FILE"
+      done
+      echo "}" >> "$VCL_FILE"
+      echo >> "$VCL_FILE"
+      echo "sub vcl_hash {" >> "$VCL_FILE"
+      IFS=";"
+      for row in $VCL_RECV
+      do
+         echo "   $row;" >> "$VCL_FILE"
+      done
+      echo "}" >> "$VCL_FILE"
+      echo >> "$VCL_FILE"
       
    echo >> "$CONFIG_FILE"
    echo "[pgbouncer]" >> "$CONFIG_FILE"
